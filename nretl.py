d150a89fce394358fe8825e114bd6940093606bd
kevin fox <kevin.j.fox@gmail.com>
2016-02-14 11:39:04 -0500

    Clean-up pass 2

--- nretl.py
+++ nretl.py
@@ -16,68 +16,12 @@ class msg_10 (Structure):
 
 class TCPHandler(SocketServer.BaseRequestHandler):
 	# TCD Socket handler
-	
-
-
-
 
 	def handle(self):
 		self.data = self.request.recv(1024).strip()
 		#print "{} wrote:".format(self.client_address[0])
 		## Dump data as ascii values
 		socket_data = self.data
-
-		'''
-		long hand
-		for x in socket_data:
-			hex_data_element = binascii.b2a_hex(x)
-			print hex_data_element
-		
-		# rewrite as list comprehension
-		hex_map = ''.join([binascii.b2a_hex(x) for x in socket_data])
-		print hex_map
-
-		print "Hex Format...."
-		
-		# Long hand
-		for y in socket_data:
-			hex_data_element = hex(ord(y))
-			print hex_data_element
-		
-		# rewrite as list comprehension
-		hex_map_format = ''.join([hex(ord(y)) for y in socket_data]) 
-		print hex_map_format
-
-		# Decode binary string into hex array. 
-		# Parse array to decode moteino data
-		# Test Message Type 10 on all Sensors
-
-		hex_map_array = [hex(ord(y)) for y in socket_data]
-		
-		hex_map_array = map(ord, socket_data)
-		print hex_map_array
-
-		try:
-
-			mote_addr = int(hex_map_array[1])
-			mote_payload_length = int(hex_map_array[2]) 
-			mote_payload_type = int(hex_map_array[3]) 
-
-			print "Recieved msg from {0}\n - Length: {1}\n - Type: {2}".format(mote_addr, mote_payload_length, mote_payload_type)
-		except:
-			pass 
-		
-		
-		Convert BIG ENDIAN Hex to LONG
-	
-		msg_long = ''.join(hex_map_array[4:])
-		
-		print msg_long
-		msg_long = '0x{0}'.format(msg_long)
-		print msg_long
-		#long_data = struct.unpack('>L', msg_long)	
-		#print long_data
-		'''
 		#print "Read Header ..."
 		try:
 			mote_header = map(ord, socket_data[:-3])
@@ -108,21 +52,13 @@ class TCPHandler(SocketServer.BaseRequestHandler):
 			
 			MILLIS_COUNT = struct.unpack('<L', '15cd5b07'.decode('hex'))[0] = 123456789
 
-		
 			'''
-
-
-			
-			#hex_map = ''.join([binascii.b2a_hex(x) for x in socket_data])
-			#print "Full: {0}".format(hex_map)
 			hex_map = ''.join([binascii.b2a_hex(x) for x in socket_data[4:]])
 			#print "Payload: {0}".format(hex_map)
 			
-			# Basic Convert from Little Endian .....
-			#
-			#START HERE
 
 			MILLIS_COUNT = struct.unpack('<L', hex_map.decode('hex'))[0]
+
 			# Format Node:Length:Type:Millis:Hex Payload
 			print "{0}:{1}:{2}:{3}:{4}".format(
 				mote_addr, 
