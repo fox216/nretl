939c632bf81484b325715966af466e16938a23bc
kevin fox <kevin.j.fox@gmail.com>
2016-02-24 21:44:32 -0500

    TODO: Fix Python Float Decoding, returning 0

--- nretl.py
+++ nretl.py
@@ -106,6 +106,8 @@ class TCPHandler(SocketServer.BaseRequestHandler):
 
 				'''
 
+				
+
 				# Format Node:Length:Type:bt,si,ui,fp,db,sc:Hex Payload
 				print "{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}".format(
 					mote_addr, 
