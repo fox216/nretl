1b6b72cf186bb15a41aaaec93c339ab7d7feed5e
kevin fox <kevin.j.fox@gmail.com>
2016-02-28 21:13:01 -0500

    Added test messages Flow python -> TCP -> Node-Red -> MotinoGateway -> Motino Sprinkle Node

--- nretl_out.py
+++ nretl_out.py
@@ -16,8 +16,12 @@ if __name__ == '__main__':
 	# Run server
 
 	HOST,PORT = "10.20.42.238", 9000
-	# Turn on zone 1 for 5 Min
-	in_byte_data = [0,64,3,20,0,5]
+	''' Turn on zone 1 for 5 Min '''
+	#in_byte_data = [0,64,3,20,0,5]
+	''' Run Front Program Run zones 1-4'''
+	in_byte_data = [0,64,3,30,70,34]
+	''' Run All Program Run zones 1-9'''
+	#in_byte_data = [0,64,3,30,65,34]
 	# convert int to hex format
 	data_raw = ''.join('{:02x}'.format(x) for x in in_byte_data)
 	# Format data stream to hex
