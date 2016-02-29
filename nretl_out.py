c26bde4e78b671a9d39c35cb0b1ad7d81e0b146f
kevin fox <kevin.j.fox@gmail.com>
2016-02-28 19:08:58 -0500

    Prototype code: enable node-red socket interface for input to sprinkler node.

---
+++ nretl_out.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# IoT Binary senor data ETL
+#
+
+import sys, json, os
+import socket
+import binascii
+import array
+import struct
+import time
+		
+		
+
+if __name__ == '__main__':
+	# Run server
+
+	HOST,PORT = "10.20.42.238", 9000
+	# Turn on zone 1 for 5 Min
+	in_byte_data = [0,64,3,20,0,5]
+	# convert int to hex format
+	data_raw = ''.join('{:02x}'.format(x) for x in in_byte_data)
+	# Format data stream to hex
+	data_hex = binascii.unhexlify(data_raw)
+	# Setup Socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# Connect 
+	sock.connect((HOST,PORT))
+	sock.sendall(data_hex)
+
+	# Close Connection
+	time.sleep(1)
+	sock.close()		
+	
+
+
