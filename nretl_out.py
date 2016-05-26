524c5ff1e646a8c438f8511ec1bee807f9eeb205
kevin fox <kevin.j.fox@gmail.com>
2016-05-25 21:06:35 -0400

    Updating API Unit tests

--- nretl_out.py
+++ nretl_out.py
@@ -14,14 +14,28 @@ import time
 
 if __name__ == '__main__':
 	# Run server
+	
 
-	HOST,PORT = "10.20.42.238", 9000
+	HOST,PORT = "10.20.42.113", 9000
 	''' Turn on zone 1 for 5 Min '''
-	#in_byte_data = [0,64,3,20,0,5]
+	#in_byte_data = [0,50,3,20,0,21]
+	in_byte_data = [0,50,3,20,1,10]
+	#in_byte_data = [0,50,3,20,2,10] 	# ok relay 2
+	#in_byte_data = [0,50,3,20,3,10]  	# ok relay 3
+	
+	#in_byte_data = [0,50,3,20,4,21]	# ok Relay 8
+	#in_byte_data = [0,50,3,20,5,21]
+	#in_byte_data = [0,50,3,20,6,21]
+	#in_byte_data = [0,50,3,20,7,21]	
+	#in_byte_data = [0,50,3,20,8,21]
+	#in_byte_data = [0,50,3,20,9,21]
+	
 	''' Run Front Program Run zones 1-4'''
-	in_byte_data = [0,64,3,30,70,34]
-	''' Run All Program Run zones 1-9'''
-	#in_byte_data = [0,64,3,30,65,34]
+	#in_byte_data = [0,64,3,30,70,34]
+	
+	''' Run All Program Run zones 1-9 for 2 Min Each'''
+	#in_byte_data = [0,50,3,30,65,5]
+	
 	# convert int to hex format
 	data_raw = ''.join('{:02x}'.format(x) for x in in_byte_data)
 	# Format data stream to hex
